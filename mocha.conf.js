eefc11d7a5c31a88e1baf13ad1d31b9d393182cd
skw622 <skw622vlato@gmail.com>
2020-05-19 20:45:48 +0300

    2nd

--- mocha.conf.js
+++ mocha.conf.js
@@ -9,10 +9,8 @@ var chai = require('chai');
 global.expect = chai.expect;
 global.assert = chai.assert;
 chai.should();
-
 // Load Sinon
 global.sinon = require('sinon');
-
 // Initialize Chai plugins
 chai.use(require('sinon-chai'));
 chai.use(require('chai-as-promised'));
